3c669d83e20e8cc2e29e36b6eae3eb22a73e0423
Naiceiry <83250687+Naiceiry@users.noreply.github.com>
2021-10-31 22:55:10 +0100

    Update models.py

--- src/models.py
+++ src/models.py
@@ -8,28 +8,44 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    name = Column(String(50), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    photo_url = Column(String(220), unique=False, nullable=True)
+    
+    
+class Publicate(Base):
+    __tablename__ = 'publicate'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
+    archive_url = Column(String(450))
+    date_publicate = Column(db.String(120), nullable=True)
+    user_id = Column(Integer, db.ForeignKey('user.id'))
+    
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    user_id_publicate = Column(Integer, db.ForeignKey('user.id'))
+    id_publicate = Column(Integer, db.ForeignKey('publicate.user_id'))
+    
+    def to_dict(self):
+        return {}
+    
+class Follow(Base):
+    __tablename__ = 'follow'
+    id = Column(Integer, primary_key=True)
+    user_id_ppl = Column(Integer, db.ForeignKey('user.id'))
+    user_id_follow = Column(Integer, db.ForeignKey('user.id'))
+    
+    def to_dict(self):
+        return {}
+    
+    
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
